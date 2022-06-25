795472295ac08f68472e544baff16c4cbd9cc54b
aguy tech <aguytech@free.fr>
2022-06-25 13:21:19 +0200

    guest/binwalk: - modify repo for squashfs4.5 - add cleaner for repo

--- guest/sub/binwalk.sh
+++ guest/sub/binwalk.sh
@@ -8,33 +8,38 @@ python3 -m pip install -U nose coverage pycryptodome pyqtgraph capstone matplotl
 
 ##### github
 
-# Install sasquatch to extract non-standard SquashFS images
-sudo apt install -y zlib1g-dev liblzma-dev liblzo2-dev
-cd /tmp && git clone https://github.com/devttys0/sasquatch
-cd sasquatch && ./build.sh
+# Install squashfs to extract non-standard SquashFS images
+cd /tmp && curl -O https://raw.githubusercontent.com/aguytech/squashfs4.5/master/build.sh
+sh build.sh
 
 # Install jefferson to extract JFFS2 file systems
 python3 -m pip install -U cstruct
 cd /tmp && git clone https://github.com/sviehb/jefferson
 cd jefferson && sudo python3 setup.py install
+cd .. && sudo rm -fR jefferson
 
 # Install ubi_reader to extract UBIFS file systems
 sudo apt install -y liblzo2-dev
 python3 -m pip install -U python-lzo
 cd /tmp && git clone https://github.com/jrspruitt/ubi_reader
 cd ubi_reader && sudo python3 setup.py install
+cd .. && sudo rm -fR ubi_reader
 
 # Install yaffshiv to extract YAFFS file systems
 cd /tmp && git clone https://github.com/devttys0/yaffshiv
 cd yaffshiv && sudo python3 setup.py install
+cd .. && sudo rm -fR yaffshiv
 
 # Install unstuff (closed source) to extract StuffIt archive files
-cd /tmp && curl -sS http://downloads.tuxfamily.org/sdtraces/stuffit520.611linux-i386.tar.gz | tar -zxv
-sudo cp bin/unstuff /usr/local/bin/
+mkdir /tmp/sdtraces
+cd /tmp/sdtraces && curl -sS http://downloads.tuxfamily.org/sdtraces/stuffit520.611linux-i386.tar.gz | tar -zxv
+sudo cp bin/* /usr/local/bin/
+cd .. && sudo rm -fR sdtraces
 
 #### binwalk
 
 cd /tmp && git clone https://github.com/ReFirmLabs/binwalk
 cd binwalk && sudo python3 setup.py install
+cd .. && sudo rm -fR binwalk
 
 binwalk -h
