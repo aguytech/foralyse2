3fa34b25cc9ebc7dfe6b32c63ef704cd4be6f2c6
aguy tech <aguytech@free.fr>
2022-06-25 13:27:10 +0200

    guest/global: - add python3 grip for markdown renderer - add engrampa - corrects swap - minor changes

--- guest/sub/global.sh
+++ guest/sub/global.sh
@@ -9,9 +9,8 @@ path=${HOME}/.local/bin
 
 ### upgrade
 
-sudo apt remove -y bluema gimp* libreoffice-* pidgin* thunderbird* transmission-gtk
+sudo apt remove -y blueman gimp* libreoffice-* pidgin* thunderbird* transmission-gtk
 sudo apt remove -y gnome-sudoku gnome-mines sgt-*
-sudo sed -i '/# deb .*partner$/ s|# ||' /etc/apt/sources.list
 sudo apt update
 sudo apt list --upgradable
 sudo apt -y dist-upgrade
@@ -19,13 +18,14 @@ sudo apt -y autoremove
 
 ##### system
 
-sudo apt install -y binutils-common bsdmainutils curl debconf-utils exfat-utils git gnupg2 gparted hfsprogs htop kpartx lnav most net-tools p7zip-full p7zip-rar pv rar sysstat testdisk tmux tree unrar vim xsysinfo # openssh-server
+sudo apt install -y binutils-common bsdmainutils curl debconf-utils exfatprogs git gnupg2 gparted hfsprogs htop kpartx lnav most net-tools p7zip-full p7zip-rar pv rar sysstat testdisk tmux tree unrar vim xsysinfo # openssh-server
 
-sudo apt install -y dconf-editor firefox-locale-fr galculator gpicview meld plank qt5ct qt5-gtk2-platformtheme thunar-media-tags-plugin tumbler-plugins-extra
+sudo apt install -y dconf-editor engrampa galculator gpicview meld plank qt5ct qt5-gtk2-platformtheme thunar-media-tags-plugin tumbler-plugins-extra
 
 ### python
 
 sudo apt-get install -y python3 python3-pip
+#python3 -m pip install -U pip grip
 
 sudo apt-get install -y python2 # python2-dev
 cd /tmp
@@ -37,7 +37,9 @@ python2 -m pip install -U pip
 
 ##### global
 
-sudo swapoff -av && sudo sh -c 'echo vm.swappiness=10 > /etc/sysctl.d/99-swappiness.conf' # limit swap
+sudo swapoff -av
+sudo sh -c 'echo vm.swappiness=10 > /etc/sysctl.d/99-swappiness.conf' # limit swap
+sudo swapon -v
 
 sudo rm /etc/localtime && sudo ln -sv /usr/share/zoneinfo/Etc/UTC /etc/localtime
 
@@ -49,9 +51,9 @@ export QT_QPA_PLATFORMTHEME=qt5ct # gtk2
 echo -e "\n# QT\nexport QT_QPA_PLATFORMTHEME=qt5ct " >> ~/.profile
 echo -e "\n#JAVA\nexport _JAVA_OPTIONS=\"-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -Dswing.crossplatformlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel \${_JAVA_OPTIONS}\"" >> ~/.profile
 
-_echoyb "After validation, set fusion / darker for qt5"
+_echoyb "After validation, adjust settings for qt5 and close qt5ct window"
 _ask
-qt5ct >/dev/null
+qt5ct 2>/dev/null
 
 ### user
 
@@ -60,7 +62,7 @@ for path in ${paths}; do
 	[ -d ${path} ] || mkdir -p ${path}
 done
 
-cp ${_PATH_SCRIPT}/conf/icons/* ~/.local/share/icons/
+cp ${_PATH_SCRIPT}/icons/* ~/.local/share/icons/
 
 ##### plank
 
@@ -79,7 +81,7 @@ StartupNotify=false
 Terminal=false
 Hidden=false' > ${path}/plank.desktop
 
-_echoyb "After validation, adjust plank preferences"
+_echoyb "After validation, adjust plank preferences and close plank window"
 _ask
 plank --preferences &
 
