cb7ed2055d345ae5690049174b6534fb0e1d0a4a
aguy tech <aguytech@free.fr>
2022-06-28 23:32:27 +0200

    guest: sub/nbd.sh minor updates

--- guest/sub/nbd.sh
+++ guest/sub/nbd.sh
@@ -6,24 +6,25 @@
 file=${_PATH_SCRIPT}/xtra/nbd.sh
 fileto=/usr/local/bin/nbd.sh
 ! [ -f ${file} ] && echo "Unable to find file: ${file}" && exit 1
-sed -i "/^_PATH_NBD=/ s|=.*$|=${_PATH_NBD}|" ${file}
 sudo cp ${file} ${fileto}
 sudo sed -i "/^_PATH_NBD=/ s|=.*$|=${_PATH_NBD}|" ${fileto}
 sudo chmod +x /usr/local/bin/nbd.sh
 
 ###  Thunar custom actions
 
-[ -d ~/.config/Thunar ] || mkdir -p ~/.config/Thunar
 file=~/.config/Thunar/uca.xml
+[ -d ~/.config/Thunar ] || mkdir -p ~/.config/Thunar
 if [ -f ${file} ]; then
-	cp -a ${file} ${file}.$(date +%s)
+	[ -f ${file}.keep ] || cp -a ${file} ${file}.keep
 else
 	echo -e '<?xml version="1.0" encoding="UTF-8"?>\n<actions>\n</actions>' > ${file}
 fi
-if ! grep -q 1655620394868230-1 ${file}; then
-	sed -i '$,1d' ${file}
-	cat ${file} conf/ca.xml > ${file}.tmp
-	mv ${file}.tmp ${file}
-fi
+
+# update already made
+grep -q 1655620394868230-1 ${file} && return
+
+sed -i '$,1d' ${file}
+cat ${file} ${_PATH_SCRIPT}/conf/ca.xml > ${file}.tmp
+mv ${file}.tmp ${file}
 
 _echoyb "logout/login from your computer to apply changes"
