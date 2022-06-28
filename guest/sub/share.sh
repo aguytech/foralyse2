46a324254dbef16c83e1f36c98636fd30e479086
aguy tech <aguytech@free.fr>
2022-06-28 23:34:10 +0200

    guest: sub/share.sh correct path share settings

--- guest/sub/share.sh
+++ guest/sub/share.sh
@@ -12,7 +12,7 @@ sudo update-initramfs -u
 
 grep -q '^/hostshare' /etc/fstab || sudo sh -c 'echo "
 # qemu share
-/hostshare                                /share        9p     noauto,trans=virtio,version=9p2000.L,rw,umask=002    0 0" >> /etc/fstab'
+/hostshare                                '${_PATH_SHARE}'        9p     noauto,trans=virtio,version=9p2000.L,rw,umask=002    0 0" >> /etc/fstab'
 
 [ -d ${_PATH_SHARE} ] || sudo mkdir -p ${_PATH_SHARE}
 sudo mount ${_PATH_SHARE}
