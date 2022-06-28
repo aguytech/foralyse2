227050d1f6f370469e6204e0da65d37d1c394457
aguy tech <aguytech@free.fr>
2022-06-28 11:36:02 +0200

    host:sub/share.sh minor updates

--- host/sub/share.sh
+++ host/sub/share.sh
@@ -12,9 +12,9 @@ _echoyb "Configure in virt-manager the shared path like this with this paramater
 echo "Source path: ${_PATH_SHARE}"
 echo "Target path: /hostshare"
 _echoyb "\nAnd add fmode and dmode by modifying XML settings options in virt-manager:"
-echo -e "filesystem type=\"mount\" accessmode=\"mapped\" ${yellow}fmode=\"0664\" dmode=\"0775\"${cclear}>
-  <source dir=\"/vms/share\"/>
-  <target dir=\"/hostshare\"/>
-  <address type=\"pci\" domain=\"0x0000\" bus=\"0x07\" slot=\"0x00\" function=\"0x0\"/>
+echo -e 'filesystem type="mount" accessmode="mapped" '${yellow}'fmode="0664" dmode="0775"'${cclear}'>
+  <source dir="/vms/share"/>
+  <target dir="/hostshare"/>
+  <address type="pci" domain="0x0000" bus="0x07" slot="0x00" function="0x0"/>
 </filesystem>'
 _ask
