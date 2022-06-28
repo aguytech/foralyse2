9d9383af84cad7d827d29a0674e6180f63143240
aguy tech <aguytech@free.fr>
2022-06-28 23:32:46 +0200

    host: sub/nbd.sh minor updates

--- host/sub/nbd.sh
+++ host/sub/nbd.sh
@@ -6,7 +6,6 @@
 file=${_PATH_SCRIPT}/xtra/nbd.sh
 fileto=/usr/local/bin/nbd.sh
 ! [ -f ${file} ] && echo "Unable to find file: ${file}" && exit 1
-sed -i "/^_PATH_NBD=/ s|=.*$|=${_PATH_NBD}|" ${file}
 sudo cp ${file} ${fileto}
 sudo sed -i "/^_PATH_NBD=/ s|=.*$|=${_PATH_NBD}|" ${fileto}
 sudo chmod +x /usr/local/bin/nbd.sh
@@ -14,15 +13,16 @@ sudo chmod +x /usr/local/bin/nbd.sh
 ###  Thunar custom actions
 
 file=~/.config/Thunar/uca.xml
-# update already made
-grep -q 1655620394868230-1 ${file} && return
-
 [ -d ~/.config/Thunar ] || mkdir -p ~/.config/Thunar
 if [ -f ${file} ]; then
 	[ -f ${file}.keep ] || cp -a ${file} ${file}.keep
 else
 	echo -e '<?xml version="1.0" encoding="UTF-8"?>\n<actions>\n</actions>' > ${file}
 fi
+
+# update already made
+grep -q 1655620394868230-1 ${file} && return
+
 sed -i '$,1d' ${file}
 cat ${file} ${_PATH_SCRIPT}/conf/ca.xml > ${file}.tmp
 mv ${file}.tmp ${file}
