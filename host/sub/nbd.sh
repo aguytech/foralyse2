069cecadf1842cfee920caf44098b41a74ba2620
aguy tech <aguytech@free.fr>
2022-06-28 11:31:11 +0200

    host:nbd update _PATH_NBD in nbd.sh script

--- host/sub/nbd.sh
+++ host/sub/nbd.sh
@@ -4,24 +4,27 @@
 
 [ -d "${_PATH_NBD}" ] || sudo mkdir -p "${_PATH_NBD}"
 file=${_PATH_SCRIPT}/xtra/nbd.sh
+fileto=/usr/local/bin/nbd.sh
 ! [ -f ${file} ] && echo "Unable to find file: ${file}" && exit 1
-sed -i "/ *path_base_nbd=/ s|=.*$|=${_PATH_NBD}|" ${file}
-sudo cp ${file} /usr/local/bin/
+sed -i "/^_PATH_NBD=/ s|=.*$|=${_PATH_NBD}|" ${file}
+sudo cp ${file} ${fileto}
+sudo sed -i "/^_PATH_NBD=/ s|=.*$|=${_PATH_NBD}|" ${fileto}
 sudo chmod +x /usr/local/bin/nbd.sh
 
 ###  Thunar custom actions
 
-[ -d ~/.config/Thunar ] || mkdir -p ~/.config/Thunar
 file=~/.config/Thunar/uca.xml
+# update already made
+grep -q 1655620394868230-1 ${file} && return
+
+[ -d ~/.config/Thunar ] || mkdir -p ~/.config/Thunar
 if [ -f ${file} ]; then
-	cp -a ${file} ${file}.$(date +%s)
+	[ -f ${file}.keep ] || cp -a ${file} ${file}.keep
 else
 	echo -e '<?xml version="1.0" encoding="UTF-8"?>\n<actions>\n</actions>' > ${file}
 fi
-if ! grep -q 1655620394868230-1 ${file}; then
-	sed -i '$,1d' ${file}
-	cat ${file} xtra/ca.xml > ${file}.tmp
-	mv ${file}.tmp ${file}
-fi
+sed -i '$,1d' ${file}
+cat ${file} ${_PATH_SCRIPT}/conf/ca.xml > ${file}.tmp
+mv ${file}.tmp ${file}
 
 _echoyb "logout/login from your computer to apply changes"
