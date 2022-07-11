e263789b3516b86d01b0de170762dc947556286a
aguy tech <aguytech@free.fr>
2022-07-11 11:20:45 +0200

    unify inc to root + remove file extension .sh for parts

--- guest/guest.sh
+++ guest/guest.sh
@@ -4,37 +4,32 @@ _PATH_BASE=$( readlink -f ${0%/*} )
 
 ### functions
 
-file=${_PATH_BASE}/sub/inc.sh
+file=${_PATH_BASE}/../inc
 ! [ -f "${file}" ] && echo "Unable to find file: ${file}" && exit 1
-! . ${file} && echo "Errors while importing ${file}" && exit 1
+! . ${file} && echo "Errors while sourcing file: ${file}" && exit 1
 
 ### begin
 
 _echoyb "- Use from the GUEST"
 
-# user conf
-_FILE_CONF=${HOME}/.config/foralyse
-[ -f "${_FILE_CONF}" ] || cp ${_PATH_BASE}/conf/foralyse ${_FILE_CONF}
-. ${_FILE_CONF}
-
 if [ -z "${_PATH_SHARE}" ]; then
-	tmp=/foralyse/share
-	_ask "Give the shared path from the Guest (${tmp}): "
-	_PATH_SHARE=${_ANSWER:-${tmp}}
+	anstmp=/foralyse/share
+	_ask "Give the shared path from the Guest (${anstmp}): "
+	_PATH_SHARE=${_ANSWER:-${anstmp}}
 	sed -i "/^_PATH_SHARE=/ s|=.*$|=${_PATH_SHARE}|" ${_FILE_CONF}
 fi
 
 if [ -z "${_PATH_CASE}" ]; then
-	tmp=/foralyse/cases
-	_ask "Give the path to mount cases (${tmp}): "
-	_PATH_CASE=${_ANSWER:-${tmp}}
+	anstmp=/foralyse/cases
+	_ask "Give the path to mount cases (${anstmp}): "
+	_PATH_CASE=${_ANSWER:-${anstmp}}
 	sed -i "/^_PATH_CASE=/ s|=.*$|=${_PATH_CASE}|" ${_FILE_CONF}
 fi
 
 if [ -z "${_PATH_NBD}" ]; then
-	tmp=/foralyse/nbd
-	_ask "Give the path to mount dumped disk (${tmp}): "
-	_PATH_NBD=${_ANSWER:-${tmp}}
+	anstmp=/foralyse/nbd
+	_ask "Give the path to mount dumped disk (${anstmp}): "
+	_PATH_NBD=${_ANSWER:-${anstmp}}
 	sed -i "/^_PATH_NBD=/ s|=.*$|=${_PATH_NBD}|" ${_FILE_CONF}
 fi
 
@@ -42,29 +37,19 @@ fi
 if grep -q "^/hostshare.*${_PATH_SHARE}" /etc/fstab \
 	&& [ -d "${_PATH_SHARE}" ] \
 	&& ! grep -q "^/hostshare.*${_PATH_SHARE}" /proc/mounts
-then sudo mount ${_PATH_SHARE}
+then
+	sudo mount ${_PATH_SHARE}
 fi
 
 ### sub
 
-_source share.sh
-_source cases.sh
-_source nbd.sh
-_source global.sh
-[ -f ${_PATH_BASE}/perso/perso.sh ]  && . ${_PATH_BASE}/perso/perso.sh
-_source forensic.sh
-_source autopsy.sh
-_source binwalk.sh
-_source regripper.sh
-_source volatility.sh
-_source wireshark.sh
-_source idafree.sh
-_source bytecode.sh
-_source luyten.sh
-_source cfr.sh
-# _source kali.sh
-#_source pandoc.sh
-_source clean.sh
+parts="share cases nbd global perso"
+parts+=" forensic autopsy binwalk regripper volatility"
+parts+=" wireshark idafree bytecode luyten cfr clean"
+# kali pandoc
+for part in ${parts}; do
+	_source ${part}
+done
 
 _echoy "\n-----------------------------------------------"
 _echoyb "This installation is complete \nRestart the guest"
--- host/host.sh
+++ host/host.sh
@@ -4,32 +4,28 @@ _PATH_BASE=$( readlink -f ${0%/*} )
 
 ### functions
 
-file=${_PATH_BASE}/sub/inc.sh
+file=${_PATH_BASE}/../inc
 ! [ -f "${file}" ] && echo "Unable to find file: ${file}" && exit 1
-! . ${file} && echo "Errors while importing ${file}" && exit 1
+! . ${file} && echo "Errors while sourcing file: ${file}" && exit 1
 
 ### begin
 
-_echoyb "- Use from the HOST"
+_echoyb "- Use from the HOST with Xubuntu 18.04 bionic already installed"
 
-# user conf
-_FILE_CONF=${HOME}/.config/foralyse
-[ -f "${_FILE_CONF}" ] || cp ${_PATH_BASE}/conf/foralyse ${_FILE_CONF}
+anstmp=/vms/share
+_ask "Give the shared path from the Host (${anstmp}): "
+_PATH_SHARE=${_ANSWER:-${anstmp}}
 
-tmp=/vms/share
-_ask "Give the shared path from the Host (${tmp}): "
-_PATH_SHARE=${_ANSWER:-${tmp}}
-
-tmp=/vms/nbd
-_ask "Give the path to mount device files (${tmp}): "
-_PATH_NBD=${_ANSWER:-${tmp}}
+anstmp=/vms/nbd
+_ask "Give the path to mount device files (${anstmp}): "
+_PATH_NBD=${_ANSWER:-${anstmp}}
 sed -i "/^_PATH_NBD=/ s|=.*$|${_PATH_NBD}|" ${_FILE_CONF}
 
 ### sub
 
-_source share.sh
-_source nbd.sh
-[ -f ${_PATH_BASE}/perso/perso.sh ] && . ${_PATH_BASE}/perso/perso.sh
+_source share
+_source nbd
+_source perso
 
 _echoy "-----------------------------------------------"
 _echoyb "The installation of Host is done \nGo into the guest to complete installation"
