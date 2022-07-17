9efc81953facd33c18f390c6784e5044ca79a4af
aguy tech <aguytech@free.fr>
2022-07-17 14:37:27 +0200

    host: Add data part

--- host/install.sh
+++ host/install.sh
@@ -20,35 +20,15 @@ file=${_PATH_REPO_BS}/inc
 ! [ -f "${file}" ] && echo "Unable to find file: ${file}" && exit 1
 ! . ${file} && echo "Errors while sourcing file: ${file}" && exit 1
 
-######################## DATA
+######################## PART
 
 _echoA "- Use from the HOST"
 
-if [ -z "${_PATH_SHARE}" ]; then
-	anstmp=/vms/share
-	_askno "Give the shared path from the Host (${anstmp})"
-	_PATH_SHARE=${_ANSWER:-${anstmp}}
-	_confset _PATH_SHARE "${_PATH_SHARE}"
-fi
+_PARTS_MAN="data share nbd perso"
 
-if [ -z "${_PATH_NBD}" ]; then
-	anstmp=/vms/nbd
-	_askno "Give the path to mount device files (${anstmp})"
-	_PATH_NBD=${_ANSWER:-${anstmp}}
-	_confset _PATH_NBD "${_PATH_NBD}"
-fi
-
-if [ -z "${_HALT}" ]; then
-	_askyn "Enable halt between each parts?"
-	_HALT=${_ANSWER}
-	_confset _HALT "${_HALT}"
-fi
-
-######################## SUB
-
-_source_sub share
-_source_sub nbd
-_source_sub perso
+for _PART in ${_PARTS_MAN}; do
+	_source_sub "${_PART}"
+done
 
 _echoa "\n-----------------------------------------------"
 _echoA "This installation of Host is done \nGo into the guest to complete installation"
